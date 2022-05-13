b543fced3b18b03ce77308f02c85198a993d538c
Igor Follador <igorledf@hotmail.com>
2022-05-13 20:10:01 -0300

    Initial Structure

---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+const bodyParser = require('body-parser');
+// const usersRoute = require('./usersRoute');
+
+module.exports = app => {
+    app.use(bodyParser.json());
+    
+    app.get('/', (req, res) => res.send('Customer Microservice'));
+    
+    // app.use(usersRoute);
+}

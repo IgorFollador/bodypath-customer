b9e4bb2d0f9cd0cff3e666cc71420de9fa295bb1
Igor Follador <igorledf@hotmail.com>
2022-05-27 23:34:08 -0300

    fix: Configuring request headers

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,13 @@ const bodyParser = require('body-parser');
 const usersRoute = require('./usersRoute');
 
 module.exports = app => {
+    app.use(function (req, res, next) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT,DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+        next();
+    });
+
     app.use(bodyParser.json());
     
     app.get('/', (req, res) => res.send('Customer Microservice'));

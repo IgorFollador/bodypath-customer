44a4cc11df14b6dd30602e79c1f858df7fe1636e
Igor Follador <igorledf@hotmail.com>
2022-06-16 23:25:38 -0300

    Update error messages in UserController

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -10,7 +10,7 @@ class UserController {
             });
             return res.status(200).json(allUsers);
         } catch (error) {
-            return res.status(500).json(error.message);
+            return res.status(500).json({ message: error.message });
         }
     }
 
@@ -19,7 +19,7 @@ class UserController {
             const allNames = await database.Users.findAll({ attributes: ['id', 'firstName', 'lastName'] });
             return res.status(200).json(allNames);
         } catch (error) {
-            return res.status(500).json(error.message);
+            return res.status(500).json({ message: error.message });
         }
     }
 
@@ -35,7 +35,7 @@ class UserController {
             user.password = null;
             return res.status(200).json(user);
         } catch (error) {
-            return res.status(500).json(error.message);
+            return res.status(500).json({ message: error.message });
         }
     }
 
@@ -51,7 +51,7 @@ class UserController {
             if(user == null) return res.status(200).json({ message: `User ${id} not found!` });
             return res.status(200).json(user);
         } catch (error) {
-            return res.status(500).json(error.message);
+            return res.status(500).json({ message: error.message });
         }
     }
 
@@ -67,7 +67,7 @@ class UserController {
             user.password = null;
             return res.status(201).json(user);
         } catch (error) {
-            return res.status(500).json(error.message);
+            return res.status(500).json({ message: error.message });
         }
     }
 
@@ -84,7 +84,7 @@ class UserController {
             })
             return res.status(200).json({ message: `ID ${id} updated` });
         } catch (error) {
-            return res.status(500).json(error.message);
+            return res.status(500).json({ message: error.message });
         }
     }
 
@@ -100,7 +100,7 @@ class UserController {
             })
             return res.status(200).json({ message: `ID ${id} deleted` });
         } catch (error) {
-            res.status(500).json(error.message);
+            res.status(500).json({ message: error.message });
         }
     }
 }
